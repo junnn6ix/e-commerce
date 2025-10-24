2b99f3e5ab5899ecf4ce6543be89fb51664d27f2
junnn6ix <ngntdluxd@gmail.com>
2025-10-24 10:49:59 +0700

    update

--- src/app/products/app.tsx
+++ src/app/products/app.tsx
@@ -1,7 +1,6 @@
 import ProductList from "@/components/ProductList";
-import Image from "next/image";
 
-const Homepage = async ({
+const ProductPage = async ({
   searchParams,
 }: {
   searchParams: Promise<{ category: string }>;
@@ -9,12 +8,9 @@ const Homepage = async ({
   const category = (await searchParams).category;
   return (
     <div className="">
-      <div className="relative aspect-[3/1] mb-12">
-        <Image src="/featured.png" alt="Featured Product" fill />
-      </div>
       <ProductList category={category} params={"homepage"} />
     </div>
   );
 };
 
-export default Homepage;
+export default ProductPage;
--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -2,6 +2,7 @@ import { ProductsType } from "@/types,";
 import Categories from "./Categories";
 import ProductCard from "./ProductCard";
 import Link from "next/link";
+import Filter from "./Filter";
 
 // TEMPORARY
 const products: ProductsType = [
@@ -125,12 +126,19 @@ const ProductList = ({
   return (
     <div className="w-full">
       <Categories />
+      {params === "homepage" && <Filter />}
 
       <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-12">
         {products.map((product) => (
           <ProductCard key={product.id} product={product} />
         ))}
       </div>
+
+      <Link
+        href={category ? `/products/?category=${category}` : "/products"}
+        className="flex justify-end mt-4 underline text-sm text-gray-500">
+        View all products
+      </Link>
     </div>
   );
 };
